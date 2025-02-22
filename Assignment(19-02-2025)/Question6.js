b701b90d406e2fb16cc101d061fd65e49ca276cc
Yash <yashthakurno.1@gmail.com>
2025-02-22 10:02:32 +0530

    uploaded assignment

---
+++ Assignment(19-02-2025)/Question6.js
@@ -0,0 +1,7 @@
+let student = {
+  name: "Alice",
+  age: 22,
+  course: "Computer Science",
+};
+let convert = JSON.stringify(student);
+console.log(convert);
--- InClassQ/Q2.js
+++ InClassQ/Q2.js
@@ -385,80 +385,348 @@
 //   let asc = str[i].charCodeAt();
 //   asc -= 96;
 //   ans += asc;
+// // }
+// // console.log(ans);
+
+// // console.log((4 + 4) % 9);
+
+// // let num = 57 - 48;
+// // console.log(((num + 4) % 9) + 48);
+
+// // let str = "All-convoYs-9-:Alert1";
+// // let key = 4;
+// // let ans = "";
+// // for (let i = 0; i < str.length; i++) {
+// //   if (str[i] == "-" || str[i] == "," || str[i] == ";" || str[i] == "%") {
+// //     ans += str[i];
+// //   } else if (str[i] >= "a" && str[i] <= "z") {
+// //     let num = str[i].charCodeAt() - 97;
+// //     ans += String.fromCharCode(((num + 4) % 26) + 97);
+// //   } else if (str[i] >= "A" && str[i] <= "Z") {
+// //     let num = str[i].charCodeAt() - 65;
+// //     ans += String.fromCharCode(((num + 4) % 26) + 65);
+// //   } else if (str[i] >= "0" && str[i] <= "9") {
+// //     let num = str[i].charCodeAt() - 48;
+// //     ans += String.fromCharCode(((num + 4) % 10) + 48);
+// //   }
+// // }
+// // console.log(ans);
+
+// // let st = [1, 2, 3, 4, 4, 4];
+// // // console.log(st.pop());
+
+// // function outer() {
+// //   let a = 10;
+// //   function inner() {
+// //     a++;
+// //     console.log(a);
+// //   }
+// //   return inner;
+// // }
+
+// // const result = outer();
+// // result();
+
+// // function memo(fn) {
+// //   let cache = {};
+// //   return function (...args) {
+// //     let argStringify = JSON.stringify(args);
+// //     if (cache[argStringify] === undefined) {
+// //       cache[argStringify] = fn(...args);
+// //     }
+// //     return cache[argStringify];
+// //   };
+// // }
+
+// // function multiply(num1, num2) {
+// //   for (let i = 0; i < 10000; i++) {}
+// //   return num1 * num2;
+// // }
+// // const effecientMul = memo(multiply);
+// // console.time("First call");
+// // console.log(effecientMul(9467, 7649));
+// // console.timeEnd("First call");
+
+// // console.time("Second call");
+// // console.log(effecientMul(9467, 7649));
+// // console.timeEnd("Second call");
+
+// // function add(a) {
+// //   return function (b) {
+// //     if (b !== undefined) {
+// //       return add(a + b);
+// //     } else {
+// //       return a;
+// //     }
+// //   };
+// // }
+
+// // console.log(add(5)(2)(3)());
+
+// let no = 111;
+// toDecimal(no);
+// function toDecimal(val) {
+//   let temp = [];
+//   while (val > 0) {
+//     temp.unshift(val % 10);
+//     val = Math.floor(val / 10);
+//   }
+//   let pow = temp.length - 1;
+//   let ans = 0;
+//   for (let i = 0; i < temp.length; i++) {
+//     ans += temp[i] * 2 ** pow;
+//     pow--;
+//   }
+//   console.log(ans);
 // }
+
+// function subString(s) {
+//   let count = 0;
+//   for (let i = 0; i < s.length; i++) {
+//     let ans = "";
+//     for (let j = i; j < s.length; j++) {
+//       ans += s[j];
+//       console.log(ans);
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+// let s = "Masai";
+// console.log(subString(s));
+
+// function subString(s) {
+//   let count = 0;
+//   for (let i = 0; i < s.length; i++) {
+//     let ans = "";
+//     for (let j = i; j < s.length; j++) {
+//       if (
+//         s[i] == "a" ||
+//         s[i] == "i" ||
+//         s[i] == "o" ||
+//         s[i] == "e" ||
+//         s[i] == "u"
+//       ) {
+//         count++;
+//       }
+//       ans += s[j];
+//       console.log(ans);
+//     }
+//   }
+//   return count;
+// }
+// let s = "abcdef";
+// console.log(subString(s));
+
+// function subString(s) {
+//   let ans = 0;
+//   for (let i = 0; i < s.length; i++) {
+//     for (let j = i; j < s.length; j++) {
+//       if (isPrime(j - i + 1)) {
+//         ans++;
+//       }
+//     }
+//   }
+//   return ans;
+// }
+
+// function isPrime(length) {
+//   if (length < 2) return false;
+//   let ans = 0;
+//   for (let i = 1; i <= length; i++) {
+//     if (length % i == 0) {
+//       ans++;
+//     }
+//   }
+//   return ans == 2;
+// }
+
+// let s = "Masai";
+// console.log(subString(s));
+
+// function subString(s, k) {
+//   let count = 0;
+//   for (let i = 0; i < s.length; i++) {
+//     for (let j = i; j < s.length; j++) {
+//       if (s[i] == k) {
+//         count++;
+//       }
+//     }
+//   }
+//   return count;
+// }
+// let s = "masai";
+// console.log(subString(s, "a"));
+
+// function subString(s) {
+//   let count = 0;
+//   for (let i = 0; i < s.length; i++) {
+//     for (let j = i; j < s.length; j++) {
+//       if (s[i] == s[j]) {
+//         count++;
+//       }
+//     }
+//   }
+//   return count;
+// }
+// let s = "masai";
+// console.log(subString(s));
+
+// function subString(s) {
+//   let count = 0;
+//   for (let i = 0; i < s.length; i++) {
+//     let sum = 0;
+//     for (let j = i; j < s.length; j++) {
+//       sum += s[j];
+//       if (sum % 2 == 0) count++;
+//     }
+//   }
+//   return count;
+// }
+// let s = [1, 2, 3, 4];
+// console.log(subString(s));
+
+// function brute(k, arr) {
+//   for (let i = 0; i < k - 1; i++) {
+//     for (let j = i + 1; j < k; j++) {
+//       if (i == j) continue;
+//       else if (arr[i] == arr[j] * 2 || arr[j] == arr[i] * 2) {
+//         console.log("Yes");
+//         return;
+//       }
+//     }
+//   }
+//   console.log("No");
+// }
+
+// brute(5, [5, 0, 9, 7, 20]);
+
+// function brute(n, arr, arr2) {
+//   let obj = {};
+//   for (let i = 0; i < n; i++) {
+//     obj[arr[i]] = (obj[arr[i]] || 0) + 1;
+//   }
+//   for (let i = 0; i < n; i++) {
+//     if (obj[arr2[i]]) {
+//       console.log(arr2[i]);
+//       return;
+//     }
+//   }
+//   console.log("No");
+// }
+
+// brute(3, [4, 5, 7], [9, 2, 5]);
+
+// function brute(n, t, arr2) {
+//   let count = 0;
+//   for (let i = 0; i < n - 1; i++) {
+//     for (let j = i + 1; j < n; j++) {
+//       if (arr2[i] + arr2[j] == t) {
+//         count++;
+//       }
+//     }
+//   }
+//   console.log(count);
+// }
+
+// brute(5, 9, [3, 0, 6, 2, 7]);
+
+// function brute(n, arr) {
+//   for (let i = 1; i < n; i++) {
+//     arr[i] += arr[i - 1];
+//   }
+
+//   for (let i = 1; i < n - 1; i++) {
+//     if (arr[n - 1] - arr[i] == arr[i - 1]) {
+//       console.log(i);
+//       return;
+//     }
+//   }
+//   console.log(-1);
+// }
+
+// // brute(7, [6, 3, 7, 2, 4, 9, 9]);
+
+// let classroom = [
+//   { id: 1, name: "Yash" },
+//   { id: 2, name: "Alice" },
+//   { id: 1, name: "Raju" },
+// ];
+
+// for (let i = 0; i < classroom.length; i++) {
+//   console.log(classroom[i].name);
+// }
+
+// let test = JSON.stringify(classroom);
+// console.log(test);
+
+// for (let i = 0; i < test.length; i++) {
+//   console.log(test[i]);
+// }
+
+// let test2 = JSON.parse(test);
+// for (let i = 0; i < test2.length; i++) {
+//   console.log(test2[i].name);
+// }
+
+// let arr = ["heelom", "dfsfef", "adsdadad"];
+// let ans = arr.sort((a, b) => {
+//   return a.localeCompare(b);
+// });
 // console.log(ans);
 
-// console.log((4 + 4) % 9);
+// function sub(str) {
+//   let max = -Infinity;
+//   for (let i = 0; i < str.length; i++) {
+//     let temp = "";
+//     for (let j = i; j < str.length; j++) {
+//       temp += str[j];
+//       max = Math.max(isMax(temp), max);
+//     }
+//   }
+//   return max;
+// }
+// function isMax(str) {
+//   if (str.length == 1) return 1;
+//   let i = 0;
+//   let j = str.length - 1;
+//   while (i < j) {
+//     if (str[i] != str[j]) {
+//       return 0;
+//     }
+//     j--;
+//     i++;
+//   }
+//   return str.length - 1;
+// }
+// //thisracecarisgood//
+// console.log(sub("thisracecarisgood"));
 
-// let num = 57 - 48;
-// console.log(((num + 4) % 9) + 48);
+// function sum(n, arr) {
+//   let max = -Infinity;
+//   for (let i = 1; i < n; i++) {
+//     arr[i] += arr[i - 1];
+//     max = Math.max(max, arr[i]);
+//   }
+//   console.log(max);
+// }
+// sum(5, [5, 4, -1, 7, 8]);
 
-// let str = "All-convoYs-9-:Alert1";
-// let key = 4;
-// let ans = "";
-// for (let i = 0; i < str.length; i++) {
-//   if (str[i] == "-" || str[i] == "," || str[i] == ";" || str[i] == "%") {
-//     ans += str[i];
-//   } else if (str[i] >= "a" && str[i] <= "z") {
-//     let num = str[i].charCodeAt() - 97;
-//     ans += String.fromCharCode(((num + 4) % 26) + 97);
-//   } else if (str[i] >= "A" && str[i] <= "Z") {
-//     let num = str[i].charCodeAt() - 65;
-//     ans += String.fromCharCode(((num + 4) % 26) + 65);
-//   } else if (str[i] >= "0" && str[i] <= "9") {
-//     let num = str[i].charCodeAt() - 48;
-//     ans += String.fromCharCode(((num + 4) % 10) + 48);
+// function perm(str) {
+//   let count = 0;
+//   for (let i = 1; i < str.length; i++) {
+//     for (let j = 0; j < i; j++) {}
 //   }
 // }
-// console.log(ans);
 
-// let st = [1, 2, 3, 4, 4, 4];
-// // console.log(st.pop());
+// let obj = {
+//   name: "RAJU",
+// };
 
-// function outer() {
-//   let a = 10;
-//   function inner() {
-//     a++;
-//     console.log(a);
-//   }
-//   return inner;
+// function greet(callback) {
+//   callback();
 // }
 
-// const result = outer();
-// result();
-
-// function memo(fn) {
-//   let cache = {};
-//   return function (...args) {
-//     let argStringify = JSON.stringify(args);
-//     if (cache[argStringify] === undefined) {
-//       cache[argStringify] = fn(...args);
-//     }
-//     return cache[argStringify];
-//   };
-// }
-
-// function multiply(num1, num2) {
-//   for (let i = 0; i < 10000; i++) {}
-//   return num1 * num2;
-// }
-// const effecientMul = memo(multiply);
-// console.time("First call");
-// console.log(effecientMul(9467, 7649));
-// console.timeEnd("First call");
-
-// console.time("Second call");
-// console.log(effecientMul(9467, 7649));
-// console.timeEnd("Second call");
-
-// function add(a) {
-//   return function (b) {
-//     if (b !== undefined) {
-//       return add(a + b);
-//     } else {
-//       return a;
-//     }
-//   };
-// }
-
-// console.log(add(5)(2)(3)());
+// greet.call(obj, () => {
+//   console.log(`Hello ${this.name}`);
+// }); // Output: Hello RAJU
